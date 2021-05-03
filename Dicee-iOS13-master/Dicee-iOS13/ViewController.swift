dc87b30c199aa79f6592d7407fa27510c9a13323
Bence Fulop <fbence90@gmail.com>
2021-05-04 09:29:08 +1000

    roll dice function changes dice image to a set one

--- Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
@@ -6,13 +6,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
-
-        diceImageView1.alpha = 0.5
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+    }
+    
 }
 
