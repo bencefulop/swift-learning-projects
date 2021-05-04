f77fbc97cb1672b37b78037032259749f215464c
Bence Fulop <fbence90@gmail.com>
2021-05-04 16:32:56 +1000

    egg timer works as intended

--- EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -8,29 +8,39 @@ class ViewController: UIViewController {
         "Hard": 12
     ]
     
-    var secondsRemaining: Int!
+    var totalTime = 0
+    var secondsPassed = 0
     
     var timer = Timer()
     
     @IBOutlet weak var titleLabel: UILabel!
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     @IBAction func hardmessSelected(_ sender: UIButton) {
+        totalTime = 0
+        secondsPassed = 0
+        progressBar.progress = 0
         titleLabel.text! = "How Do You Like Your Eggs?"
         timer.invalidate()
         
+        
         let hardness = sender.currentTitle!
-        secondsRemaining = eggtimes[hardness]!
+        totalTime = eggtimes[hardness]!
+        
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(upDateTimer), userInfo: nil, repeats: true )
-   
+        
     }
     
     @objc func upDateTimer() {
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining!) seconds")
-            secondsRemaining -= 1
+        if secondsPassed < totalTime {
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+            progressBar.progress = Float(percentageProgress)
+            secondsPassed += 1
         } else {
             timer.invalidate()
+            progressBar.progress = 1
             titleLabel.text! += "\n Done 🍳"
         }
     }
