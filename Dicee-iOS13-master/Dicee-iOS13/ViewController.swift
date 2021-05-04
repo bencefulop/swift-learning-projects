70757411f58523d38260bb10d8fbdbd52a93444e
Bence Fulop <fbence90@gmail.com>
2021-05-04 10:51:47 +1000

    2 dice random roll on btn tap

--- Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13-master/Dicee-iOS13/ViewController.swift
@@ -2,15 +2,18 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
-
+    
     @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        let dicesArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        // the below two lines to the same thing.
+        diceImageView1.image = dicesArray.randomElement()
+        diceImageView2.image = dicesArray[Int.random(in: 0...5)]
     }
     
 }
