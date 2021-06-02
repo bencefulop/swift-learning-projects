90f8fd8ca75dd125a1063636ae90b6998356e8c8
Bence Fulop <fbence90@gmail.com>
2021-06-02 21:14:48 +1000

    flashcaht init and display title char by char

---
+++ Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = ""
+        var characterIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        
+        for letter in titleText {
+            print(0.1 * characterIndex)
+            print("----")
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * characterIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            characterIndex += 1
+        }
+        
+        
+    }
+    
+    
+}
