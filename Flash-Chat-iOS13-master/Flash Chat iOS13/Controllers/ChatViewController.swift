8fb1155871501f5e1cc8c3dd46dbbcb9b6fa44cb
Bence Fulop <fbence90@gmail.com>
2021-06-14 16:28:47 +1000

    add firestore and db stoing

--- Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,6 +14,8 @@ class ChatViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    let db = Firestore.firestore()
+    
     var messages: [Message] = [
         Message(sender: "1@2.com", body: "Hey"),
         Message(sender: "a@b.com", body: "Hello"),
@@ -30,6 +32,18 @@ class ChatViewController: UIViewController, UITableViewDataSource {
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection(Constants.FStore.collectionName).addDocument(data: [
+                Constants.FStore.senderField : messageSender,
+                Constants.FStore.bodyField : messageBody
+            ]) { (error) in
+                if let e = error {
+                    print("there was an issue saving data to firestore, \(e)")
+                } else {
+                    print("successfully saved data")
+                }
+            }
+        }
     }
     
 
