4f1187ce2830c97e9e0bb7f1ce8ac13bf3ab004f
Bence Fulop <bence.fulop@arq.group>
2021-07-04 17:50:43 +1000

    business card basic layout with photo

---
+++ SwiftUI BusinessCard/SwiftUI BusinessCard/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  SwiftUI BusinessCard
+//
+//  Created by Bence Fulop on 4/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("b&wAvatar")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                Text("Bence Fulop")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI BusinessCard/SwiftUI BusinessCard/SwiftUI_BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BusinessCardApp.swift
+//  SwiftUI BusinessCard
+//
+//  Created by Bence Fulop on 4/7/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
