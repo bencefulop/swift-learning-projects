238e4f41c5aaa5e63118a212522dd2ae655f4e71
Bence Fulop <fbence90@gmail.com>
2021-07-17 12:22:56 +1000

    display list items from api call

--- HackerNews-SwiftUI/HackerNews-SwiftUI/ContentView.swift
+++ HackerNews-SwiftUI/HackerNews-SwiftUI/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
     var body: some View {
         NavigationView {
-            List(posts) { post in
-                Text(post.title)
+            List(networkManager.posts) { post in
+                HStack {
+                    Text("\(post.points)")
+                    Text(post.title)
+                }
             }
             .navigationBarTitle("H4X0R NEWS")
+        }
+        .onAppear {
+            networkManager.fetchData()
         }
     }
 }
@@ -23,9 +32,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-let posts = [
-    Post(id: "1", title: "Hello"),
-    Post(id: "2", title: "Bonjour"),
-    Post(id: "3", title: "Hola")
-]
