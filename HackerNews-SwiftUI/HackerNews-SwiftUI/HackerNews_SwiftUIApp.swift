550f21af4842bf6bb5b2c2e1712232c50f57e880
Bence Fulop <bence.fulop@arq.group>
2021-07-17 11:26:41 +1000

    list view using closure

---
+++ HackerNews-SwiftUI/HackerNews-SwiftUI/HackerNews_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNews_SwiftUIApp.swift
+//  HackerNews-SwiftUI
+//
+//  Created by Bence Fulop on 17/7/21.
+//
+
+import SwiftUI
+
+@main
+struct HackerNews_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
