e28b9b63c984338ea633fa7810f49623ac2407f6
Bence Fulop <fbence90@gmail.com>
2021-07-17 15:42:37 +1000

    add tableview harcoded content

---
+++ Todoey-iOS13-master/Todoey/TodoListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = [
+        "Find Mike",
+        "Buy eggos",
+        "Destroy Demogorgon"
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - TableView Datasource Methods
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    // MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        print(itemArray[indexPath.row])
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
