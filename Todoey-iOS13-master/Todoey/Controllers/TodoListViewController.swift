b43d5ede5d64242bf723efcbe65803c31cf10b1f
Bence Fulop <fbence90@gmail.com>
2021-08-01 12:53:59 +1000

    copy CoreData code to appdelagete

--- Todoey-iOS13-master/Todoey/Controllers/TodoListViewController.swift
+++ Todoey-iOS13-master/Todoey/Controllers/TodoListViewController.swift
@@ -26,38 +26,27 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        
         cell.textLabel?.text = itemArray[indexPath.row].title
-        
         cell.accessoryType = itemArray[indexPath.row].done! ? .checkmark : .none
-        
         return cell
     }
     
     // MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done!
-        
         saveItems()
-        
         tableView.reloadData()
-        
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
     @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
         var textField = UITextField()
-        
         let alert = UIAlertController(title: "Add new Todoey item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add item", style: .default) { action in
             guard let newItem = textField.text else {return}
-            
             self.itemArray.append(Item(title: newItem))
-            
             self.saveItems()
-            
             self.tableView.reloadData()
         }
         
@@ -67,9 +56,7 @@ class TodoListViewController: UITableViewController {
         }
         
         alert.addAction(action)
-        
         present(alert, animated: true, completion: nil)
-        
     }
 
     func saveItems() {
