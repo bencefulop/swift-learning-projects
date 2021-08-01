a411d8180b602bb2912d810d5c4875fa0e2dabcd
Bence Fulop <fbence90@gmail.com>
2021-08-01 18:09:13 +1000

    implement delete action

--- Todoey-iOS13-master/Todoey/Controllers/TodoListViewController.swift
+++ Todoey-iOS13-master/Todoey/Controllers/TodoListViewController.swift
@@ -37,9 +37,11 @@ class TodoListViewController: UITableViewController {
     // MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+//        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        context.delete(itemArray[indexPath.row])
+        itemArray.remove(at: indexPath.row)
         saveItems()
-        tableView.reloadData()
+
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -71,6 +73,7 @@ class TodoListViewController: UITableViewController {
         } catch {
             print("error saving item:\(error)")
         }
+        tableView.reloadData()
     }
     
     func loadItems() {
