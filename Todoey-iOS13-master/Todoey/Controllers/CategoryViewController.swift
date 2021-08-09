ad8ec6831ecec1f322c77586ca81e315bbee47f6
Bence Fulop <fbence90@gmail.com>
2021-08-09 21:22:54 +1000

    create category view controller

---
+++ Todoey-iOS13-master/Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Bence Fulop on 9/8/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    //MARK: - TableViewDataSource Methods
+    
+}
+
