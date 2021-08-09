128ef7b44b3c780a832c945517108235d65dbd2c
Bence Fulop <fbence90@gmail.com>
2021-08-10 08:32:37 +1000

    add compoundPredicates to filter items by their categories

--- Todoey-iOS13-master/Todoey/Controllers/CategoryViewController.swift
+++ Todoey-iOS13-master/Todoey/Controllers/CategoryViewController.swift
@@ -75,8 +75,18 @@ class CategoryViewController: UITableViewController {
     // MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // we'll need to segue here to the appropriate category
+        
+        performSegue(withIdentifier: "goToItems", sender: self)
+        
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categoryArray[indexPath.row]
+        }
     }
 }
 
