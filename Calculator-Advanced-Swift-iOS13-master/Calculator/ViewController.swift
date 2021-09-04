9100d542e22e59b2db2ec0d08c05da0f6d25ca7a
Bence Fulop <fbence90@gmail.com>
2021-09-04 15:30:31 +1000

    refactor with computed properties

--- Calculator-Advanced-Swift-iOS13-master/Calculator/ViewController.swift
+++ Calculator-Advanced-Swift-iOS13-master/Calculator/ViewController.swift
@@ -14,26 +14,33 @@ class ViewController: UIViewController {
     
     private var isFinishedTypingNumber: Bool = true
     
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("cannot convert display label text to a Double")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         isFinishedTypingNumber = true
-        
-        guard let number = Double(displayLabel.text!) else {
-            fatalError("cannot convert display label text to a Double")
-        }
-        
+
         if let calcMethod = sender.currentTitle {
             switch calcMethod {
             case "+/-":
-                displayLabel.text = String(number * -1)
+                displayValue *= -1
             case "AC":
                 displayLabel.text = "0"
             case "%":
-                displayLabel.text = String(number / 100)
+                displayValue /= 100
             default:
                 print("an unknown button was pressed")
             }
         }
-        
     }
 
     
@@ -45,20 +52,14 @@ class ViewController: UIViewController {
             } else {
                 
                 if numValue == "." {
-                    guard let currentDisplayValue = Double(displayLabel.text!) else {
-                        fatalError("number cannot be converted to a Double")
-                    }
-                    let isInt = floor(currentDisplayValue) == Double(displayLabel.text!)!
+                    let isInt = floor(displayValue) == displayValue
                     if !isInt {
                         return
                     }
                 }
-                
                 displayLabel.text = displayLabel.text! + numValue
             }
         }
-        
     }
-
 }
 
